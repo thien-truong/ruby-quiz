9880a202147d49b007f79057f63162e0d982b8b6
Thien Truong <bluetigon11@yahoo.com>
2012-09-09 23:30:13 -0500

    Prepare message.
    Discard any non A to Z characters, and uppercase all remaining letters.
    Split the message into five character groups, using Xs to pad the last
    group, if needed.

--- encryptor.rb
+++ encryptor.rb
@@ -4,4 +4,22 @@ class Encryptor
   def initialize(message)
     @message = message
   end
+
+  def prepare_message
+    @message.gsub! /[^a-zA-Z]/, ''
+    @message.upcase!
+    character_count = 1
+    new_message = ""
+    @message.each_char do |character|
+      new_message << character
+      if character_count % 5 == 0
+        new_message << ' '
+      end
+      character_count += 1
+    end
+    (5 - @message.length % 5).times do
+      new_message << 'X' 
+    end
+    @message = new_message
+  end
 end
--- encryptor_test.rb
+++ encryptor_test.rb
@@ -5,6 +5,13 @@ class TestEncryptor < Test::Unit::TestCase
   def test_has_message
     message = "This is a test."
     encryptor = Encryptor.new(message)
-    assert_equal(encryptor.message, message)
+    assert_equal(message, encryptor.message)
+  end
+
+  def test_prepares_message
+    message = "This is a test."
+    encryptor = Encryptor.new(message)
+    encryptor.prepare_message
+    assert_equal("THISI SATES TXXXX", encryptor.message)
   end
 end
