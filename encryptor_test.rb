38a90b5921288925f919b1f4eb18e3fed01d0fff
Thien Truong <bluetigon11@yahoo.com>
2012-09-09 23:35:03 -0500

    Add setup method to encryptor_test.

--- encryptor_test.rb
+++ encryptor_test.rb
@@ -2,16 +2,17 @@ require "test/unit"
 require_relative "encryptor"
 
 class TestEncryptor < Test::Unit::TestCase
+  def setup
+    @message = "This is a test."
+    @encryptor = Encryptor.new(@message)
+  end
+
   def test_has_message
-    message = "This is a test."
-    encryptor = Encryptor.new(message)
-    assert_equal(message, encryptor.message)
+    assert_equal(@message, @encryptor.message)
   end
 
   def test_prepares_message
-    message = "This is a test."
-    encryptor = Encryptor.new(message)
-    encryptor.prepare_message
-    assert_equal("THISI SATES TXXXX", encryptor.message)
+    @encryptor.prepare_message
+    assert_equal("THISI SATES TXXXX", @encryptor.message)
   end
 end
