d1202e0a5435c6510191737526a14b3424e2a1ed
Thien Truong <bluetigon11@yahoo.com>
2012-09-15 22:27:51 -0500

    Add setup method to deck_test.

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -2,14 +2,16 @@ require "test/unit"
 require_relative "../lib/deck"
 
 class TestDeck < Test::Unit::TestCase
+  def setup
+    @deck = Deck.new(Array((1..54)))
+  end
+
   def test_has_order
-    deck = Deck.new(Array((1..54)))
-    assert_equal(Array((1..54)), deck.order)
+    assert_equal(Array((1..54)), @deck.order)
   end
 
   def test_moves_joker_down_once
-    deck = Deck.new(Array((1..54)))
-    deck.move_down!(53)
-    assert_equal(Array((1..52)) << 54 << 53, deck.order)
+    @deck.move_down!(53)
+    assert_equal(Array((1..52)) << 54 << 53, @deck.order)
   end
 end
