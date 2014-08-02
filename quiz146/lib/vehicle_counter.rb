8d3bdfdecaed5b1606a6e9de3f2a34c6dd9855d7
Thien Truong <moments1ever+pub@gmail.com>
2014-08-02 11:53:35 -0500

    Refactor.

--- quiz146/lib/vehicle_counter.rb
+++ quiz146/lib/vehicle_counter.rb
@@ -1,29 +1,34 @@
 class VehicleCounter
   def count_total_vehicles(input_file)
     counter = 0
-    already_read_a_line = false
-    skip_lines = 0
+    skip_lines = 1
 
     until input_file.eof
       current_line = input_file.readline
-
-      we_want_to_process_the_line = already_read_a_line && skip_lines == 0
-      if we_want_to_process_the_line
-        north_or_south_bound = current_line.chr
-        case north_or_south_bound
-          when 'A'
-            counter = counter + 1
-            already_read_a_line = false
-          when 'B'
-            counter = counter + 1
-            skip_lines = 2
-        end
+      if skip_lines == 0
+        skip_lines = determine_lines_to_skip(current_line)
+        counter += 1
       else
-        already_read_a_line = true
         skip_lines -= 1 if skip_lines > 0
       end
     end
 
     counter
+  end
+
+  private
+
+  def determine_lines_to_skip(current_line)
+    directional_sensor = current_line.chr
+    car_is_going_north = directional_sensor == 'A'
+    car_is_going_south = directional_sensor == 'B'
+    if car_is_going_north
+      skip_lines = 1
+    elsif car_is_going_south
+      skip_lines = 2
+    else
+      raise 'Encountered invalid sensor name. Expected A or B.'
+    end
+    skip_lines
   end
 end
--- quiz146/test/vehicle_counter_spec.rb
+++ quiz146/test/vehicle_counter_spec.rb
@@ -3,9 +3,9 @@ require_relative '../lib/vehicle_counter'
 describe VehicleCounter do
   it 'counts the total number of vehicles going Northbound - recorded with one sensor' do
     vehicle_counter = VehicleCounter.new
-    input = StringIO.new("A98186\nA98333\nA499718\nA499886\n", 'r')
-    expect(vehicle_counter.count_total_vehicles(input)).to eq(2)
-    input.close
+    StringIO.open("A98186\nA98333\nA499718\nA499886\n", 'r') do |input|
+      expect(vehicle_counter.count_total_vehicles(input)).to eq(2)
+    end
   end
 
   it 'counts the total number of vehicles going Southbound - recorded with two sensor' do
@@ -17,10 +17,16 @@ describe VehicleCounter do
 
   it 'counts the total number of vehicles when a vehicle is interrupted at eof' do
     vehicle_counter = VehicleCounter.new
-    input = StringIO.new("A98186\nA98333\nA499718\n", 'r')
-    expect(vehicle_counter.count_total_vehicles(input)).to eq(1)
-    input.close
+    StringIO.open("A98186\nA98333\nA499718\n", 'r') do |input|
+      expect(vehicle_counter.count_total_vehicles(input)).to eq(1)
+    end
   end
 
+  it 'raises an exception when the vehicle sensor name is invalid' do
+    vehicle_counter = VehicleCounter.new
+    StringIO.open("A638379\nC638382\n", 'r') do |input|
+      expect { vehicle_counter.count_total_vehicles(input) }.to raise_error(RuntimeError)
+    end
+  end
 
 end
