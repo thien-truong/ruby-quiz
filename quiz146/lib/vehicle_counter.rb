c75cd76abb686324678386ec9f160e101d4ab184
Thien Truong <moments1ever+pub@gmail.com>
2015-01-07 15:49:25 +0530

    Count vehicles per 12 hours for northbound stream.

--- quiz146/lib/vehicle_counter.rb
+++ quiz146/lib/vehicle_counter.rb
@@ -1,7 +1,35 @@
 require_relative 'parser'
+require_relative 'vehicle_streams'
+require_relative 'vehicle'
 
 class VehicleCounter
-  def count_vehicles(vehicle_stream)
-    vehicle_stream.count
+  def count_northbound_vehicles(vehicle_stream, interval)
+    max_milliseconds = interval
+    current_day = 1
+    output_array = []
+    vehicle_count = 0
+
+    vehicle_stream.northbound_vehicles.each do |vehicle|
+
+      if vehicle.end_time >= max_milliseconds
+        max_milliseconds = max_milliseconds + interval
+        output_array << vehicle_count
+        vehicle_count = 0
+      end
+
+      if vehicle.day_recorded != current_day
+        current_day = vehicle.day_recorded
+        max_milliseconds = interval
+        output_array << vehicle_count
+        vehicle_count = 0
+      end
+
+      vehicle_count = vehicle_count + 1
+
+    end
+
+    output_array << vehicle_count
+
+    output_array
   end
 end
--- quiz146/test/vehicle_counter_spec.rb
+++ quiz146/test/vehicle_counter_spec.rb
@@ -1,11 +1,22 @@
 require_relative '../lib/vehicle_counter'
-require_relative '../lib/parser'
+require_relative '../lib/vehicle_streams'
 require_relative '../lib/vehicle'
 
-describe VehicleCounter do
-  it 'counts the total vehicles in a stream' do
-    vehicle_stream = [Vehicle.new(1234, 5678), Vehicle.new(2345, 6789)]
-    vehicle_counter = VehicleCounter.new
-    expect(vehicle_counter.count_vehicles(vehicle_stream)).to eq(2)
+
+describe(VehicleCounter) do
+  before(:each) do
+    @vehicle_streams = VehicleStreams.new
+    @vehicle_streams.add_northbound_vehicle(Vehicle.new(1, 98186, 98333))
+    @vehicle_streams.add_northbound_vehicle(Vehicle.new(1, 1035674, 1035821))
+    @vehicle_streams.add_northbound_vehicle(Vehicle.new(1, 47089414, 47089557))
+    @vehicle_streams.add_northbound_vehicle(Vehicle.new(2, 98186, 98333))
+    @vehicle_streams.add_northbound_vehicle(Vehicle.new(2, 47089414, 47089557))
   end
+
+  it 'counts northbound vehicles per 12 hours' do
+    vehicle_counter = VehicleCounter.new
+    milliseconds_per_12_hours = 43200000
+    expect(vehicle_counter.count_northbound_vehicles(@vehicle_streams, milliseconds_per_12_hours)).to eq([2, 1, 1, 1])
+  end
+
 end
